cc9581f1feb05670762330e8a25de943662a4a27
MQ4 <mq4@t7er.com>
2020-03-25 12:16:55 +0800

    修改函数名字

--- map.go
+++ map.go
@@ -1,26 +1,28 @@
 package common
 
-import "strconv"
+import (
+	"strconv"
+)
 
 var boolDict = map[string]bool{"t": true, "true": true}
 
 type Map map[string]string
 
-func (m Map) Bool(name string, def bool) bool {
+func (m Map) BoolDefault(name string, def bool) bool {
 	if val, ok := m[name]; ok {
 		return boolDict[val]
 	}
 	return def
 }
 
-func (m Map) String(name, def string) string {
+func (m Map) StringDefault(name, def string) string {
 	if val, ok := m[name]; ok {
 		return val
 	}
 	return def
 }
 
-func (m Map) Int(name string, def int) int {
+func (m Map) IntDefault(name string, def int) int {
 	if val, ok := m[name]; ok {
 		if v, e := strconv.Atoi(val); e == nil {
 			return v
@@ -29,7 +31,7 @@ func (m Map) Int(name string, def int) int {
 	return def
 }
 
-func (m Map) Int64(name string, def int64) int64 {
+func (m Map) Int64Default(name string, def int64) int64 {
 	if val, ok := m[name]; ok {
 		if i, e := strconv.ParseInt(val, 10, 64); e == nil {
 			return i
@@ -38,7 +40,7 @@ func (m Map) Int64(name string, def int64) int64 {
 	return def
 }
 
-func (m Map) Int32(name string, def int32) int32 {
+func (m Map) Int32Default(name string, def int32) int32 {
 	if val, ok := m[name]; ok {
 		if i, e := strconv.ParseInt(val, 10, 64); e == nil {
 			return int32(i)
@@ -47,7 +49,7 @@ func (m Map) Int32(name string, def int32) int32 {
 	return def
 }
 
-func (m Map) UInt64(name string, def uint64) uint64 {
+func (m Map) UInt64Default(name string, def uint64) uint64 {
 	if val, ok := m[name]; ok {
 		if i, e := strconv.ParseUint(val, 10, 64); e == nil {
 			return i
@@ -56,7 +58,7 @@ func (m Map) UInt64(name string, def uint64) uint64 {
 	return def
 }
 
-func (m Map) UInt32(name string, def uint32) uint32 {
+func (m Map) UInt32Default(name string, def uint32) uint32 {
 	if val, ok := m[name]; ok {
 		if i, e := strconv.ParseUint(val, 10, 64); e == nil {
 			return uint32(i)
